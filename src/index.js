e63037f0038a90c885c0a3f6b04f13e929e6f9c1
Natalia Kuczma <natalia.kuczma@makingwaves.com>
2019-05-21 09:58:50 +0200

    add lab.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 'use strict'
 import './css/index.css';
+const lab = require('lab.js/dist/lab.dev.js');
 
+// Define a component
+var experiment = new lab.flow.Sequence({
+    content: [
+      new lab.html.Screen({
+        content: 'The experiment is running!',
+      }),
+    ],
+  })
+
+// Run it
+experiment.run();
 
 console.log("I'm alive");
